8133672f757357f706c7f6f6b8529996ccef563f
wilsonify <tom.andrew.wilson@gmail.com>
2024-09-14 13:05:24 -0400

    restore.sh

--- restore/restore.sh
+++ restore/restore.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec "thirtybees-docker-compose-mysql-1" mariadb --user=$MYSQL_ROOT_USER --password=$MYSQL_ROOT_PASSWORD --xml "thirtybees" < "mariadb_backup_20240914094546.xml"
+docker exec "thirtybees-docker-compose-mysql-1" mariadb --user=$MYSQL_ROOT_USER --password=$MYSQL_ROOT_PASSWORD --xml "thirtybees" < "/var/www/default/mariadb_backup_20240914094546.xml"
